d06c4a83350b9e5b53930191f541f274dbf9c1fc
MaksimSamorokovskii <139316826+MaksimSamorokovskii@users.noreply.github.com>
2024-03-05 11:26:44 +0700

    create

---
+++ ThoughtTrove/ThoughtTroveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThoughtTroveApp.swift
+//  ThoughtTrove
+//
+//  Created by Максим Самороковский on 05.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ThoughtTroveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
